cafa64abc81afd1733190419c4011591df98275e
chinmai <allada.chinmai@providence.org>
2025-02-19 22:39:59 +0530

    fixed onclick of profilepic

--- mangashelf/src/components/logo/index.tsx
+++ mangashelf/src/components/logo/index.tsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
 import LogoIcon from '../../assets/logos/manga.png'
 
 
@@ -12,14 +13,14 @@ const Logo = () => {
     }, []);
     
     return (
-        <a href='/' className='flex self-center gap-4'>
+        <Link to="/" className='flex self-center gap-4'>
             <img 
                 src={profilePic || LogoIcon} 
                 className={`w-10 h-10 ${profilePic !=='' ? 'rounded-full' : ''} object-cover`} 
                 alt="Profile"
             />
             <h1 className="text-2xl font-bold self-center">Manga Shelf</h1>
-        </a>
+        </Link>
 
     );
 };
